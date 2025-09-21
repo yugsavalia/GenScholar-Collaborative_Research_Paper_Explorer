49f0ba9197dfaf874d6554c5147137f17cc74b3f
Dhruvil Patel <202301201@dau.ac.in>
2025-09-21 21:33:19 +0530

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -14,3 +14,13 @@ def signup_view(request):
         form = UserCreationForm()
     return render(request, "accounts/signup.html", {"form": form})
 
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("dashboard")
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form": form})
