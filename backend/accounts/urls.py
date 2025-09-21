1d99a4e30f05fa13e447c433d061e5513f2b4944
Manan <146871724+mananghonia@users.noreply.github.com>
2025-09-21 21:35:27 +0530

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("signup/", views.signup_view, name="signup"),
+    path("logout/", views.logout_view, name="logout"),
     path("login/", views.login_view, name="login"),
 ]
+
 
