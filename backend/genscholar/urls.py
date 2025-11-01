6bcdeefbdd7ff5584425730431e158217290facc
mananghoia <mananghoia@gmail.com>
2025-11-01 19:01:13 +0530

    Refactor: Renamed all 'cursortest' to 'genscholar'

--- backend/genscholar/urls.py
+++ backend/genscholar/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cursortest project.
+URL configuration for genscholar project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
