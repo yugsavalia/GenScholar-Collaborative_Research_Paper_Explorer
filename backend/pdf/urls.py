a1be8636594f6b4bba49edf548465702fcd1b47a
Dhruvil Patel <202301201@dau.ac.in>
2025-11-07 15:15:18 +0530

    added deleted pdf feature

--- backend/pdf/urls.py
+++ backend/pdf/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('workspace/<int:workspace_id>/upload/', views.upload_pdf_view, name='upload_pdf'),
     path('<int:pdf_id>/view/', views.view_pdf_view, name='view_pdf'),
     path('<int:pdf_id>/serve/', views.serve_pdf_view, name='serve_pdf'),
+    path('<int:pdf_id>/delete/', views.delete_pdf_view, name='delete_pdf'),
+
 ]
 
 
