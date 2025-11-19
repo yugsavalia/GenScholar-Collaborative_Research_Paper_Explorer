ca0fcc7036d27647a832f28db726a0eabf9a39b9
Archan-1710 <202301217@dau.ac.in>
2025-11-19 22:25:19 +0530

    Revert "Integrated frontend and backend"

--- backend/pdfs/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PdfsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pdfs'
-
-    def ready(self):
-        import pdfs.signals  # <-- ADD THIS LINE to register the signals
--- backend/run_server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Django server with Daphne for WebSocket support...
-daphne -b 0.0.0.0 -p 8000 cursortest.asgi:application
-
-
-
