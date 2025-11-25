c5a5659f0e734688b5e7c52c1e2697ca5c7b114a
yugsavalia <144619699+yugsavalia@users.noreply.github.com>
2025-11-25 16:12:19 +0530

    Updated manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#manage.py
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -20,3 +21,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
